001cff1ac7cdf149edefb20d8977bd6e18949400
PRR184 <raghavendra18042002@gmail.com>
2022-07-05 01:22:49 +0530

    1)Authentication Added for routes 2)Login, Logout, Profile Route Created

--- online_judge/problems/views.py
+++ online_judge/problems/views.py
@@ -1,6 +1,5 @@
-from http.client import HTTPResponse
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 from .models import Problem
 
@@ -13,9 +12,11 @@ def home(request):
     }
     return render(request, 'problems/home.html',context)
 
+@login_required
 def details(request,problem_id):
     return render(request, 'problems/details.html',{"id":problem_id})
 
+@login_required
 def submissions(request):
     return render(request, 'problems/submissions.html')
 
