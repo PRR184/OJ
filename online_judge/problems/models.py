73089288524a072a977bd5c4d2026b44bf34edd5
PRR184 <raghavendra18042002@gmail.com>
2022-07-04 23:35:27 +0530

    Problem and Submission Model Created

--- online_judge/problems/models.py
+++ online_judge/problems/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.utils import timezone
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Problem(models.Model):
+    title = models.CharField(max_length=30,unique=True)
+    content = models.TextField()
+    difficulty = models.CharField(max_length=20)
+    inputTestCasePath = models.CharField(max_length=200)
+    outputTestCasePath = models.CharField(max_length=200)
+    
+
+class Submission(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    problem = models.ForeignKey(Problem,on_delete=models.CASCADE)
+    verdict = models.CharField(max_length=30);
+    submitted_time = models.DateTimeField(default=timezone.now)
+    submitted_code = models.TextField()
+
+
