98d430264bae55a3b2cf04f1fe9a5700664f47f0
PRR184 <raghavendra18042002@gmail.com>
2022-07-04 23:40:33 +0530

    Users App Created

--- online_judge/online_judge/settings.py
+++ online_judge/online_judge/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'problems.apps.ProblemsConfig',
+    'users.app.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
