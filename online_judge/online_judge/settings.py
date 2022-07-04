1fc1adf0cdf511dcc3dca31998065dfb717dbb16
PRR184 <raghavendra18042002@gmail.com>
2022-07-05 00:13:34 +0530

    User Register Form and Register Route created using crispy-forms library

--- online_judge/online_judge/settings.py
+++ online_judge/online_judge/settings.py
@@ -32,7 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'problems.apps.ProblemsConfig',
-    'users.app.UsersConfig',
+    'users.apps.UsersConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -123,3 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
